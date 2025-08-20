14278a3131fd4f24d26243c114427e4e4afcded2
blyu413 <bofanl413@gmail.com>
2025-08-20 19:14:19 +0700

    add back description

--- metasim/cfg/tasks/maniskill/push_cube_cfg.py
+++ metasim/cfg/tasks/maniskill/push_cube_cfg.py
@@ -36,7 +36,10 @@ class PushCubeCfg(ManiskillTaskCfg):
     ### official_url:
     https://maniskill.readthedocs.io/en/latest/tasks/table_top_gripper/index.html#pushcube-v1
     ### platforms:
-
+    - mujoco
+    - isaaclab
+    ### video_url:
+    push_cube.mp4
     ### notes:
     """
 
